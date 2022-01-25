1225b435038915c197d209de9d676ce1b153df3e
kandrakotanagavasishta@gmail.com <kandrakotanagavasishta@gmail.com>
2022-01-25 14:50:11 +0530

    Git_A work

--- src/My_proje/My_Class.java
+++ src/My_proje/My_Class.java
@@ -5,6 +5,8 @@ public class My_Class {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("Namaste");
+		System.out.println("salam-e-vaalakum");
+		
 	}
 
 }
